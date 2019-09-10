42da79bcbc584f563d44f1c0552c9afadfa7482e
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-09-10 19:11:24 +0800

    fixup config error

--- assets/startup.sh
+++ assets/startup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ "$URI" != "" ]; then
-  ARGS="-Dspring.cloud.config.uri=$URI"${ARGS:+" $ARGS"}
+  ARGS="-Dspring.cloud.config.server.git.uri=$URI"${ARGS:+" $ARGS"}
 fi
 
 if [ "$USERNAME" != "" ]; then
-  ARGS="-Dspring.cloud.config.username=$USERNAME"${ARGS:+" $ARGS"}
+  ARGS="-Dspring.cloud.config.server.git.username=$USERNAME"${ARGS:+" $ARGS"}
 fi
 
 if [ "$PASSWORD" != "" ]; then
-  ARGS="-Dspring.cloud.config.password=$PASSWORD"${ARGS:+" $ARGS"}
+  ARGS="-Dspring.cloud.config.server.git.password=$PASSWORD"${ARGS:+" $ARGS"}
 fi
 
 if [ -f /app/app.jar ]; then
