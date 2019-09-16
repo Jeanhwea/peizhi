8c4ad27e74df7de8c9fbfea388f044a761e3c6d0
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-09-16 16:43:20 +0800

    repack

--- src/main/java/com/avic/mti/peizhi/Application.java
+++ src/main/java/com/avic/mti/peizhi/Application.java
@@ -1,7 +1,5 @@
 package com.avic.mti.peizhi;
 
-import java.util.TimeZone;
-import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -10,15 +8,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class Application {
 
-  private static final String PEIZHI_TIME_ZONE = "Asia/Shanghai";
-
   public static void main(String[] args) {
-    TimeZone.setDefault(TimeZone.getTimeZone(PEIZHI_TIME_ZONE));
     SpringApplication.run(Application.class, args);
   }
 
-  @PostConstruct
-  public void setDefaultTimezone() {
-    TimeZone.setDefault(TimeZone.getTimeZone(PEIZHI_TIME_ZONE));
-  }
 }
